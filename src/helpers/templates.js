642d86ebe04131a7fc3e5ad7e87d9813ce2c6cab
Jorge Vargas <jorgevargasla@gmail.com>
2021-11-16 10:41:11 -0500

    Fix image url

--- src/helpers/templates.js
+++ src/helpers/templates.js
@@ -58,47 +58,51 @@ export const buildTemplate = ({
   return element;
 };
 
-export const speakerTemplate = (speaker) => ({
-  tag: 'article',
-  className: ['d-grid', 'gap-1', 'speaker'],
-  children: [
-    {
-      tag: 'div',
-      className: 'left',
-      children: [
-        {
-          tag: 'img',
-          className: 'picture',
-          attributes: {
-            src: speaker.imageUrl,
-            alt: speaker.name,
+export const speakerTemplate = (speaker) => {
+  const image = new URL(speaker.imageUrl, import.meta.url).href;
+
+  return {
+    tag: 'article',
+    className: ['d-grid', 'gap-1', 'speaker'],
+    children: [
+      {
+        tag: 'div',
+        className: 'left',
+        children: [
+          {
+            tag: 'img',
+            className: 'picture',
+            attributes: {
+              src: image,
+              alt: speaker.name,
+            },
           },
-        },
-      ],
-    },
-    {
-      tag: 'div',
-      className: 'right',
-      children: [
-        {
-          tag: 'h3',
-          className: ['font-size-h3', 'name'],
-          textContent: speaker.name,
-        },
-        {
-          tag: 'p',
-          className: ['font-size-h6', 'subtitle'],
-          textContent: speaker.subtitle,
-        },
-        {
-          tag: 'p',
-          className: 'description',
-          textContent: speaker.description,
-        },
-      ],
-    },
-  ],
-});
+        ],
+      },
+      {
+        tag: 'div',
+        className: 'right',
+        children: [
+          {
+            tag: 'h3',
+            className: ['font-size-h3', 'name'],
+            textContent: speaker.name,
+          },
+          {
+            tag: 'p',
+            className: ['font-size-h6', 'subtitle'],
+            textContent: speaker.subtitle,
+          },
+          {
+            tag: 'p',
+            className: 'description',
+            textContent: speaker.description,
+          },
+        ],
+      },
+    ],
+  };
+};
 
 export const speakersTemplate = (speakers) => {
   const people = {
