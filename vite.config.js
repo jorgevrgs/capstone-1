d82b8f3723b829aaafb347f0192688609fc9a6e8
Jorge Vargas <jorgevargasla@gmail.com>
2021-11-16 17:57:26 -0500

    Update settings to use multi page

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,28 @@ import { resolve } from 'path';
 import autoprefixer from 'autoprefixer';
 // import packageInfo from './package.json';
 
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
 /**
  * @type {import('vite').UserConfig}
  */
 const config = {
   // base: process.env.NODE_ENV === 'production' ? `/${packageInfo.name}/` : '/',
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about', 'index.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': resolve(__dirname, '/src'),
+      '@': root,
       '~': resolve(__dirname, '/node_modules'),
     },
   },
