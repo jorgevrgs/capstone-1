fb177341462ab4998576c7a988b03e348ce4ce53
Jorge Vargas <jorgevargasla@gmail.com>
2021-11-16 10:09:46 -0500

    Fix repository link

--- .deploy.sh
+++ .deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:jorgevrgs/portfolio.git main:deploy
+git push -f git@github.com:jorgevrgs/capstone-1.git main:deploy
 
 cd -
